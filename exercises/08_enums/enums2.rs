8eaac021972c44a5ebdedcfdb46e6ab99c4da327
arsal <arsalanhussain547@gmail.com>
2025-09-07 01:15:03 +0500

    Worked on enums

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -7,11 +7,16 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize { width: u64, height: u64 },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
     fn call(&self) {
-        println!("{self:?}");
+        println!("{self:#?}");
     }
 }
 
