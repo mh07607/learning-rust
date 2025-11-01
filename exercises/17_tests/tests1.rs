6c8abc899aa2b4d7638a0453331fdef9d571460e
arsal <arsalanhussain547@gmail.com>
2025-11-01 15:17:03 +0500

    Completed tests exercises

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -13,11 +13,14 @@ fn main() {
 mod tests {
     // TODO: Import `is_even`. You can use a wildcard to import everything in
     // the outer module.
+    use is_even;
 
     #[test]
+    #[should_panic]
     fn you_can_assert() {
         // TODO: Test the function `is_even` with some values.
-        assert!();
-        assert!();
+
+        assert!(is_even(23));        
+        assert!(is_even(1023));
     }
 }
