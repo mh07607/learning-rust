967c8f99428b39906be50e3abeb954a4f2c6d0f6
arsal <arsalanhussain547@gmail.com>
2025-11-18 18:54:12 +0500

    Refactored and handled errors instead of panicing in minigrep project

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let filename: &String = &args[2];
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for '{}' in file '{}'", query, filename);
+    println!("Searching for '{}' in file '{}'", config.query, config.file_path);
 
-    let contents: String = fs::read_to_string(filename)
-        .expect(&format!("Not able to read file '{}'", filename));
+    let contents: String = fs::read_to_string(&config.file_path)
+        .expect(&format!("Not able to read file '{}'", config.file_path));
 
     println!("File Content: \n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments. Usage: <program> <query> <file_path>");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
